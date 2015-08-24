aa451d9844e7a4c21f3be78cbec4f706860e8fe8
Darren Cauthon <darren@cauthon.com>
2015-08-23 20:37:41 -0500

    Refactor.

--- lib/twitter_huginn_agents/lost_follower.rb
+++ lib/twitter_huginn_agents/lost_follower.rb
@@ -1,36 +1,32 @@
 require 'twitter'
 
 module TwitterHuginnAgents
+
   class LostFollower < HuginnAgent
+
     def self.description
       'Track lost followers on Twitter'
     end
 
     def check
-      (previous_followers - current_followers).each do |follower|
-        create_event payload: twitter_client.user(follower).to_hash
+      followers_who_were_not_here_at_last_check.each do |follower|
+        create_event payload: user_data_for(follower)
       end
       memory[:followers] = current_followers
     end
 
-    def current_followers
-      twitter_client.follower_ids(twitter_username).to_hash[:ids]
-    end
-
-    def previous_followers
-      memory[:followers] || []
-    end
-
-    def twitter_username
-      options[:twitter_username]
-    end
-
     def default_options
       {
-        consumer_key: '',
-        consumer_secret: '',
+        consumer_key:     '',
+        consumer_secret:  '',
         twitter_username: '',
       }
+    end
+
+    def validate_options
+      errors.add :base, 'you must provide your Twitter consumer key' if options['consumer_key'].to_s.strip == ''
+      errors.add :base, 'you must provide your Twitter consumer secret' if options['consumer_secret'].to_s.strip == ''
+      errors.add :base, 'you must provide your Twitter username' if options['twitter_username'].to_s.strip == ''
     end
 
     def twitter_client
@@ -40,10 +36,26 @@ module TwitterHuginnAgents
       end
     end
 
-    def validate_options
-      errors.add :base, 'you must provide your Twitter consumer key' if options['consumer_key'].to_s.strip == ''
-      errors.add :base, 'you must provide your Twitter consumer secret' if options['consumer_secret'].to_s.strip == ''
-      errors.add :base, 'you must provide your Twitter username' if options['twitter_username'].to_s.strip == ''
+    def twitter_username
+      options[:twitter_username]
     end
+
+    def current_followers
+      twitter_client.follower_ids(twitter_username).to_hash[:ids]
+    end
+
+    def followers_who_were_not_here_at_last_check
+      previous_followers - current_followers
+    end
+
+    def user_data_for id
+      twitter_client.user(id).to_hash
+    end
+
+    def previous_followers
+      memory[:followers] || []
+    end
+
   end
+
 end
